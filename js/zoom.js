2450544f8881b3666b45de8b26a05c613837364c
Mohd Asad <mohammed.asadullah@foodhub.com>
2025-11-10 21:34:48 +0530

    Deployed 639078c with MkDocs version: 1.6.1

--- js/zoom.js
+++
@@ -1,8 +0,0 @@
-// docs/js/zoom.js
-// Load medium-zoom from CDN dynamically
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/medium-zoom@1/dist/medium-zoom.min.js';
-script.onload = () => {
-    mediumZoom('img');
-};
-document.head.appendChild(script);
